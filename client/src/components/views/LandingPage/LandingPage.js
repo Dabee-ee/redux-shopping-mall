d6bfaa9b2ee79e01333ca5751d0adedc425e1294
Dabee-ee <movie2529@naver.com>
2022-09-08 19:45:18 +0900

    ✨ Add 상품 목록 조회 기능

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,73 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { FaCode } from "react-icons/fa";
+import axios from "axios";
+import { Icon, Col, Card, Row, Carousel } from "antd";
+import Meta from "antd/lib/card/Meta";
+import ImageSlider from "../../utils/ImageSlider";
 
 function LandingPage() {
+  const [Products, setProducts] = useState([]);
+  const [Skip, setSkip] = useState(0);
+  const [Limit, setLimit] = useState(8);
+  const [PostSize, setPostSize] = useState(0);
+
+  useEffect(() => {
+    let body = {
+      skip: Skip,
+      limit: Limit,
+    };
+
+    getProducts(body);
+  }, []);
+
+  const getProducts = (body) => {
+    axios.post("/api/product/products", body).then((response) => {
+      if (response.data.success) {
+        if (body.loadMore) {
+          setProducts([...Products, ...response.data.productInfo]);
+        } else {
+          setProducts(response.data.productInfo);
+        }
+        setPostSize(response.data.postSize);
+      } else {
+        alert(" 상품들을 가져오는데 실패 했습니다.");
+      }
+    });
+  };
+
+  const renderCards = Products.map((product, index) => {
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
-    )
+      <Col lg={6} md={8} xs={24} key={index}>
+        <Card
+          cover={
+            <a href={`/product/${product._id}`}>
+              <ImageSlider images={product.images} />
+            </a>
+          }
+        >
+          <Meta title={product.title} description={`$${product.price}`} />
+        </Card>
+      </Col>
+    );
+  });
+
+  return (
+    <div style={{ width: "75%", margin: "3rem auto" }}>
+      <div style={{ textAlign: "center" }}>
+        <h2>
+          Let's Travel Anywhere <Icon type="rocket" />{" "}
+        </h2>
+      </div>
+
+      {/* Filter */}
+
+      {/* Search */}
+
+      {/* Cards */}
+
+      <Row gutter={[16, 16]}>{renderCards}</Row>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
